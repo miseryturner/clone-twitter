b847ef42a8105c48ceedf733ba3ca4db611f9e63
Misery Turner <59616944+miseryturner@users.noreply.github.com>
2023-12-08 16:29:33 +0500

    diff-format for date

--- resources/views/ideas/shared/idea_card.blade.php
+++ resources/views/ideas/shared/idea_card.blade.php
@@ -48,10 +48,10 @@
             @include('ideas.shared.like-button')
             <div>
                 <span class="fs-6 fw-light text-muted"> <span class="fas fa-clock"> </span>
-                    {{ $idea->created_at }} 
+                    {{ $idea->created_at->diffForHumans() }} 
                 </span>
             </div>
         </div>
-        @include('shared.comments_box')
+        @include('ideas.shared.comments_box')
     </div>
 </div>
